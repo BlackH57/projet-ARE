c8468b3b08b625632dcae774ecc90ddbf2d6d021
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-04-19 21:14:43 +0200

    add chronometre
    Ajout d'une fonction permettant d'avoir un chronomètre. Le décompte du temps se fait dans le sens croissant.

--- Fonction utile.py
+++ Fonction utile.py
@@ -5,3 +5,14 @@ def dict_to_list(L):
         for j in i:
             L_res.append(j)
     return L_res
+
+
+
+from time import *
+
+def chronometre(sec):
+    for i in range(0,sec+1):
+        print(i)
+        sleep(1)
+    print('STOP')
+    return 1
